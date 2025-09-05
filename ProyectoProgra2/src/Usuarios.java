10cd81114cc3ea65a22cc4ea8fcbd15ead1c2cdf
andrewest-andrew <andreslaurav@gmail.com>
2025-09-05 07:57:27 -0400

    Update Usuarios.java

--- ProyectoProgra2/src/Usuarios.java
+++ ProyectoProgra2/src/Usuarios.java
@@ -1,17 +1,48 @@
 public class Usuarios {
-    int id;
-    String nombre;
-    String apellido;
-    String correo;
-    int rni;
-    String password;
 
-public Usuarios () {
-    this.id=id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private int RNI;
+    private String password;
+
+public Usuarios (String nombre, String apellido, String correo, int RNI, String password) {
+
     this.nombre = nombre;
     this.apellido = apellido;
-    this.correo=correo;
-    this.rni = rni;
-    this.password=password;
+    this.correo= correo;
+    this.RNI = RNI;
+    this.password= password;
+
+
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public int getRNI() {
+        return RNI;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", correo='" + correo + '\'' +
+                ", RNI=" + RNI +
+                '}';
+}
 }
