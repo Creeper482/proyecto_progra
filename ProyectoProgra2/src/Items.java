92ce9bdffcaf807dae90348bd7db6cb2c3220e06
andrewest-andrew <andreslaurav@gmail.com>
2025-09-08 09:18:55 -0400

    Update Items.java

--- ProyectoProgra2/src/Items.java
+++ ProyectoProgra2/src/Items.java
@@ -1,55 +1,43 @@
-public class Items {
-    private int idItem;
-    private String nItem;
+class Items {
+    private static int contadorId = 1;
+
+    private int Id;
+    private String nombreItem;
     private Double PU;
     private String unidades;
-    private String creadoPor;
 
-    public Items(int idItem,String nItem, double PU, String unidades, String creadoPor){
-        this.idItem=idItem;
-        this.nItem=nItem;
-        this.PU=PU;
-        this.unidades=unidades;
-        this.creadoPor=creadoPor;
+
+    public Items(String nombreItem, double PU, String unidades) {
+        this.Id = contadorId++;
+        this.nombreItem = nombreItem;
+        this.PU = PU;
+        this.unidades = unidades;
+
     }
 
-    public int getIdItem() {
-        return idItem;
+    public int getId() {
+        return Id;
     }
 
-    public void setIdItem(int idItem) {
-        this.idItem = idItem;
-    }
-
-    public String getItem() {
-        return nItem;
-    }
-
-    public void setItem(String item) {
-        nItem = item;
+    public String getNombreItem() {
+        return nombreItem;
     }
 
     public Double getPU() {
         return PU;
     }
 
-    public void setPU(Double PU) {
-        this.PU = PU;
-    }
-
     public String getUnidades() {
         return unidades;
     }
 
-    public void setUnidades(String unidades) {
-        this.unidades = unidades;
-    }
-
-    public String getCreadoPor() {
-        return creadoPor;
-    }
-
-    public void setCreadoPor(String creadoPor) {
-        this.creadoPor = creadoPor;
+    @Override
+    public String toString() {
+        return "Item{" +
+                "Id=" + Id +
+                ", nombreItem='" + nombreItem + '\'' +
+                ", precioUnitario=" + PU +
+                ", unidades=" + unidades +
+                '}';
     }
 }
